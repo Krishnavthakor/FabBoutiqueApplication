429ed21d6191bcd23795e8710e5c0f1fd8eec229
krishnathakorkt <kthakor8445@conestogac.on.ca>
2022-06-29 19:24:59 -0400

    Top and Bottom Toolbar commit. Home Page UI First Commit.

--- app/src/main/java/com/example/fabboutique/MainActivity.java
+++ app/src/main/java/com/example/fabboutique/MainActivity.java
@@ -1,14 +1,45 @@
 package com.example.fabboutique;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
 
 public class MainActivity extends AppCompatActivity {
 
+    WishlistFragment fragment_wishlist =new WishlistFragment();
+    HomeFragment fragment_home=new HomeFragment();
+
+    BottomNavigationView bottomNavigationView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        bottomNavigationView=findViewById(R.id.bottom_navigation);
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,fragment_home).commit();
+
+        bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+                switch(item.getItemId())
+                {
+                    case R.id.home:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,fragment_home).commit();
+                        return true;
+                    case R.id.wishlist:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,fragment_wishlist).commit();
+                        return true;
+
+                }
+                return false;
+            }
+        });
     }
 }
