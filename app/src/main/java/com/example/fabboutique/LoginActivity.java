9cf9238e80ad43d28ad871e2c14d1364b4850b0e
krishnathakorkt <kthakor8445@conestogac.on.ca>
2022-07-13 21:28:00 -0400

    Login and Registration construction done(encryption left for password)

--- app/src/main/java/com/example/fabboutique/LoginActivity.java
+++ app/src/main/java/com/example/fabboutique/LoginActivity.java
@@ -3,31 +3,78 @@ package com.example.fabboutique;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.example.fabboutique.DataBaseHandler.DataBaseHandler;
+import com.example.fabboutique.Models.ProductCategory;
+import com.example.fabboutique.Models.User;
 
 public class LoginActivity extends AppCompatActivity {
 
-    private Button btnSignUp;
+    private Button btnSignUp,btnLogin;
+    EditText edtUsername, edtPassword;
 
     //creating database instance
-    SQLiteDatabase db;
+    DataBaseHandler dbh;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        btnSignUp=(Button) findViewById(R.id.btnSignUp);
+        dbh=new DataBaseHandler(getApplicationContext());
+        btnSignUp = (Button) findViewById(R.id.btnSignUp);
+        btnLogin = (Button) findViewById(R.id.btnLogin);
+        edtUsername = (EditText) findViewById(R.id.editTextEmail);
+        edtPassword = (EditText) findViewById(R.id.editTextPassword);
 
-        //creating database named "fabboutique"
-        db=openOrCreateDatabase("FabBoutique",MODE_PRIVATE,null);
-
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Cursor cursor = dbh.searchData(edtUsername.getText().toString(), edtPassword.getText().toString());
+                //if its null then display toast message
+                if (cursor.getCount() == 0) {
+                    Toast.makeText(LoginActivity.this, "Username or Password doesn't match", Toast.LENGTH_LONG).show();
+                } else {
+                    if (cursor.moveToFirst()) {
+                            //initialize recipe class and set data and store object to arraylist
+                            User user = new User();
+                            user.setUserType(cursor.getString(6));
+                        cursor.close();
+                        dbh.close();
+                            if(user.getUserType().equals("customer"))
+                            {
+                                Toast.makeText(LoginActivity.this, "Login Successful!", Toast.LENGTH_LONG).show();
+                                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                                // added data to intent using value-pair, can access data using productDetails
+                                // intent.putExtra("username", edtUsername.getText().toString());
+                                // switch to the ContentsActivity
+                                startActivity(intent);
+                            }
+                            else
+                            {
+                                Toast.makeText(LoginActivity.this, "Login Successful!", Toast.LENGTH_LONG).show();
+                                Intent intent = new Intent(LoginActivity.this, AdminDashboard.class);
+                                // added data to intent using value-pair, can access data using productDetails
+                                // intent.putExtra("username", edtUsername.getText().toString());
+                                // switch to the ContentsActivity
+                                startActivity(intent);
+                            }
+                    }
+                }
+            }
+        });
         btnSignUp.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                Intent intent = new Intent(LoginActivity.this, RegistrationActivity.class);
+                startActivity(intent);
             }
         });
     }
--- app/src/main/java/com/example/fabboutique/RegistrationActivity.java
+++ app/src/main/java/com/example/fabboutique/RegistrationActivity.java
@@ -3,6 +3,7 @@ package com.example.fabboutique;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.util.Patterns;
@@ -12,6 +13,8 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.example.fabboutique.DataBaseHandler.DataBaseHandler;
+import com.example.fabboutique.Models.User;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
@@ -26,7 +29,7 @@ public class RegistrationActivity extends AppCompatActivity {
     Button btnSignUp;
 
     //declaring db instance
-    SQLiteDatabase dbh;
+    DataBaseHandler dbh;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -43,13 +46,6 @@ public class RegistrationActivity extends AppCompatActivity {
         //focus on this field during start up
         edtFirstName.requestFocus();
 
-        //creating or opening database
-        dbh=openOrCreateDatabase("FabBoutique",MODE_PRIVATE,null);
-
-        //creating a table for users
-        dbh.execSQL("CREATE TABLE IF NOT EXISTS USERS(userid integer,email varchar,password password, firstname varchar,lastname varchar,telephone integer)");
-
-
         btnSignUp.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -59,7 +55,7 @@ public class RegistrationActivity extends AppCompatActivity {
             private void registerMethod() {
                 String firstName = edtFirstName.getText().toString().trim();
                 String lastName = edtLastName.getText().toString().trim();
-                String phoneNumber = edtPhoneNumber.getText().toString().trim();
+                String phoneNumber= edtPhoneNumber.getText().toString();
                 String email = edtEmail.getText().toString().trim();
                 String password = edtPassword.getText().toString().trim();
 
@@ -101,13 +97,26 @@ public class RegistrationActivity extends AppCompatActivity {
                     return;
                 }
 
+                dbh=new DataBaseHandler(RegistrationActivity.this  );
+                //inserting user to database
+                User user = new User( firstName,lastName,email,password,phoneNumber,"customer");
 
+                boolean insertState = dbh.registerUser(user);
 
+                if (insertState) {
+                    Toast.makeText(RegistrationActivity.this, "User Registered Successfully! Now you can do Login", Toast.LENGTH_LONG).show();
+                    Intent loginIntent = new Intent(RegistrationActivity.this, LoginActivity.class);
+                    startActivity(loginIntent);
+                } else {
+                    Toast.makeText(RegistrationActivity.this, "Registration Failed!", Toast.LENGTH_LONG).show();
+                }
             }
         });
     }
 
     public void btnLogin(View view) {
+        Intent intent = new Intent(RegistrationActivity.this, LoginActivity.class);
+        startActivity(intent);
 
     }
 }
